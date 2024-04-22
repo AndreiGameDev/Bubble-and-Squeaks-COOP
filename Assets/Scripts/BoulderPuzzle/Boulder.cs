c37d799f32f15e8ae25160f6a6e41e19a0493708
Andrei <49598916+AndreiGama@users.noreply.github.com>
2024-04-22 16:17:31 +0100

    Updated Boulder Mechanic
    Polished so the player can't go inside the boulder when stationary and the player cannot push either when stationary. It still has no collision while travelling.

--- Assets/Scripts/BoulderPuzzle/Boulder.cs
+++ Assets/Scripts/BoulderPuzzle/Boulder.cs
@@ -1,16 +1,20 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Boulder : MonoBehaviour, IInteractable {
     Rigidbody2D rb;
-    Collider2D col;
+    
     [SerializeField] float speed = 4;
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<Collider2D>();
     }
 
-    IEnumerator MoveBoulder(DirFacing direction, WizardType magicType) {
+    IEnumerator MoveBoulder(DirFacing direction, WizardType magicType, bool hasRunAlready) {
+        if(gameObject.layer != LayerMask.NameToLayer("Obstacles")) {
+            gameObject.layer = LayerMask.NameToLayer("Obstacles");
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
         Vector3 lastPos = transform.position;
         float pushForce = speed;
         if(magicType == WizardType.Dark) {
@@ -18,32 +22,42 @@ public class Boulder : MonoBehaviour, IInteractable {
         }
         switch(direction) {
             case DirFacing.Left:
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
                 rb.velocity = Vector2.left * pushForce;
                 break;
             case DirFacing.Right:
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
                 rb.velocity = Vector2.right * pushForce;
                 break;
             case DirFacing.Up:
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
                 rb.velocity = Vector2.up * pushForce;
                 break;
             case DirFacing.Down:
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
                 rb.velocity = Vector2.down * pushForce;
                 break;
         }
         yield return new WaitForFixedUpdate();
-        if(Vector3.Distance(lastPos, transform.position) >= 0) {
-            StartCoroutine(MoveBoulder(direction, magicType));
+        // Need to wait one update in order to make sure the boulder has at least one frame to move it's position otherwise it stops velocity imedietally.
+        if(hasRunAlready) {
+            if(Vector3.Distance(lastPos, transform.position) > 0) {
+                StartCoroutine(MoveBoulder(direction, magicType, true));
+            } else {
+                rb.velocity = Vector2.zero;
+                gameObject.layer = LayerMask.NameToLayer("Default");
+                rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
         } else {
-            rb.velocity = Vector2.zero;
+            StartCoroutine(MoveBoulder(direction,magicType, true));
         }
-
     }
     public void Interact(PlayerRefferenceMaster player, DirFacing? direction = null) {
         if(!direction.HasValue) {
             Debug.LogError("Error, no direction input");
         } else {
             Debug.Log("Push");
-            StartCoroutine(MoveBoulder(direction.Value, player.wizzardMagicType));
+            StartCoroutine(MoveBoulder(direction.Value, player.wizzardMagicType, false));
         }
     }
 
